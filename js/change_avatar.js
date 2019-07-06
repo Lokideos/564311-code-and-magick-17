95447e145814b610195c2fb3555803a05d386d5d
lokideos <archmage09@gmail.com>
2019-07-06 14:46:47 +0300

    User can change his avatar in setup section

---
+++ js/change_avatar.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function () {
+  // Initialize
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  // Selected DOM elements
+  var setupSection = document.querySelector('.setup');
+  var preview = setupSection.querySelector('.setup-user-pic');
+  var fileUploader = setupSection.querySelector('input[type=file]');
+
+  fileUploader.addEventListener('change', function () {
+    var file = fileUploader.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (extension) {
+      return fileName.endsWith(extension);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+})();
